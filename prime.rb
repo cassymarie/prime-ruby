133e5f1ddc452ae7ad49b991b3010c4ef730152b
cassymarie <cassy2205@gmail.com>
2020-10-21 19:36:33 -0600

    commit tree c252a2f56b98d818a6362c32f683ab92eeba207a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+
+def prime?(num)
+    if num <= 1 
+        return false
+    elsif num == 2 || num == 3
+        return true
+    elsif num.even?
+        return false
+    else
+        arr_num = *(3..Math.sqrt(num).floor)
+        arr_num.each {|x| 
+            if (num % x) == 0 
+                return false
+            end}
+            return true
+    end
+end
